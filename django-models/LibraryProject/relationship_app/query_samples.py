b55e978f8faf5562b577dd7fbc8301147b4b2c97
Sandfin Nasio <nasiosandfin@gmail.com>
2026-01-25 23:02:20 +0700

    Fix query_samples.py with explicit Author and Library lookups

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,7 +8,8 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-def books_by_author(author):
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
 
 # List all books in a library
