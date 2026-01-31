7d6b56c7cc87748c3e1d126be1c0ebb7c591807b
Sandfin Nasio <nasiosandfin@gmail.com>
2026-01-31 08:28:13 +0700

    Updated urls.py to use views.register, LoginView, and LogoutView with templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,10 +8,18 @@ urlpatterns = [
 from django.urls import path
 from .views import list_books, LibraryDetailView, register, user_login, user_logout
 
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views   # so we can reference views.register
+
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),
-    path("login/", user_login, name="login"),
-    path("logout/", user_logout, name="logout"),
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
+
+
+    
+
