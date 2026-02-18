62f23199dada9332dad384dd96e920664523b5fa
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-18 09:44:44 +0300

    Fix imports for DRF filtering, search, and ordering in BookListView

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 from django_filters import rest_framework as filters
 from .models import Book
 from .serializers import BookSerializer
-
 
 class BookListView(generics.ListAPIView):
     """
