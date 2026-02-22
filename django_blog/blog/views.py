c8609d60c97201838a0853f54586b9b8b49410ef
Sandfin Nasio <nasiosandfin@gmail.com>
2026-02-22 15:22:59 +0300

    Integrate django-taggit, add TagWidget to PostForm, implement search and tag views with URLs

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -172,3 +172,13 @@ class TagPostsView(ListView):
     def get_queryset(self):
         return Post.objects.filter(tags__name=self.kwargs['tag'])
 
+from django.views.generic import ListView
+from .models import Post
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/tag_posts.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['tag_slug'])
